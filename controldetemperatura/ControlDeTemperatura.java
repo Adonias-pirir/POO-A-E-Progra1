127f3194f78f6b3827306fb6a0cf225a745968f2
Adonias Pirir <josuepirir111@gmail.com>
2025-02-23 23:58:20 -0600

    Ejercicio 2

---
+++ controldetemperatura/ControlDeTemperatura.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package controldetemperatura;
+
+/**
+ *
+ * @author Dell Latitude
+ */
+public class ControlDeTemperatura {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Termometro temp = new Termometro(25);
+        
+        System.out.println("La temperatura es: " + temp.getTemperatura());
+        
+        temp.conversionFahrenheit();
+        temp.conversionKelvin();
+        
+        temp.setTemperatura(18);
+        System.out.println("Nueva temperatura: " + temp.getTemperatura());
+        temp.conversionFahrenheit();
+        temp.conversionKelvin();
+    }
+    
+}
